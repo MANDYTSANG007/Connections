8107565a0bc6e7c3702b4d46e72951bf15e5201a
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-23 07:44:22 -0800

    add createdAt, username, and reactions properties in thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -7,5 +7,21 @@ var ThoughtSchema = new schema({
         required: true,
         minLength: 1,
         maxLength: 280,
-    }
-})
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        //use a getter method to format the timestamp on query
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    //array of nested documents created with the reactionSchema
+    reactions:[
+        reactionSchema
+    ],
+});
+
+//TODO:
+// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
