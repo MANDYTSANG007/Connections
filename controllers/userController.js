3cf5364c99c933c93b1e67abbb0372456650f108
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-24 00:17:25 -0800

    add deleteUser route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 // Import User model
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
     getAllUsers(req, res) {
@@ -34,9 +34,23 @@ module.exports = {
                 ? res
                     .status(404)
                     .json({ message: "No user found with that ID"})
-                : res.json(user)
+                :res.json(user)
             )
             .catch((err) => res.status(500).json(err));
+    },
+    deleteUser(req, res){
+        User.findOneAndDelete(
+            { _id: req.params.id }
+        )
+            .then((user) =>
+            !user
+                ?res
+                    .status(404)
+                    .json({ message: "No user with that ID"})
+                :Thought.deleteMany({ _id: { $in: user.thoughts }})
+            )
+            .then(() => res.json({ message: 'User and thoughts deleted!'}))
+            .catch((err) => res.status(500).json(err)); 
     },
 };
 
