9060417fab2ae54d2efc9be26d481bb2503937d4
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-23 23:55:51 -0800

    add createUser and updateUser routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,45 @@
-const { User, Thought } = require('../models');
+// Import User model
+const { User } = require('../models');
+
+module.exports = {
+    getAllUsers(req, res) {
+        User.find()
+            .then((users)=> res.json(users))
+            .catch((err)=> res.status(500).json(err));
+    },
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.id })
+            .populate(thought)
+            .populate(friend)
+            .then((user)=>
+                !user
+                    ? res
+                        .status(404)
+                        .json({ message: "No user with that ID"})
+                    : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    createUser(req, res){
+        User.create(req.body)
+        .then((dbUserData) => res.json(dbUserData))
+        .catch((err) => res.status(500).json(err));
+    },
+    updateUser(req, res){
+        User.findOneAndUpdate(
+            { _id: req.params.id},
+        )
+            .then((user)=>
+            !user
+                ? res
+                    .status(404)
+                    .json({ message: "No user found with that ID"})
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+};
+
 
 
 
