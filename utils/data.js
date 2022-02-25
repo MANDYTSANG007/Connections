d2731f7a3acbf6a53144728dd835ec6876325bd8
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-25 15:57:14 -0800

    add utils folder and set up data.js and seed.js files

---
+++ utils/data.js
@@ -0,0 +1,18 @@
+const username = [
+    "Margaret",
+    "Albert",
+    "John"
+];
+
+const email = [
+    "margaretH@gmail.com",
+    "albertE@gmail.com",
+    "johnW@gmail.com"
+];
+
+const thought = [
+    "Don't let fear get in the way and don't be afraid to say 'I don't know' or 'I don't understand'.",
+    "The true sign of intelligence is not knowledge but imagination.",
+    "It takes time to create excellence. If it cold be done quicklym more people would do it.",
+]
+
