3e35c81f49e63fd9bcd4035f8f65353fac945527
Mandy Tsang <mandy.tsang007@gmail.com>
2022-02-24 16:08:15 -0800

    add updateThought and deleteThought routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -33,6 +33,28 @@ module.exports = {
                 )
             .catch((err)=> res.status(500).json(err));
     },
+    updateThought(req, res){
+        Thought.findOneAndUpdate(
+            { _id: req.params.id},
+        )
+            .then((thought)=>
+            !thought
+                ?res.status(400).json({ message: 'No thought with that ID'})
+                : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteThought(req, res){
+        Thought.findOneAndDelete(
+            { _id: req.params.id }
+        )
+            .then((thought) =>
+            !thought
+                ?res.status(404).json({ message: 'No thought with that ID'})
+                :res.json(thought)
+            )
+            .catch((err)=> res.status(500).json(err));
+    },
 
 }
 
